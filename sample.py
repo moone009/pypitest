b6b00205b1426ec402a7f6677627638287049f26
unknown <w47593@PC78253.we.dirsrv.com>
2017-03-10 10:44:29 -0600

    testing

---
+++ sample.py
@@ -0,0 +1,18 @@
+
+class test:
+    def __init__(self,int):
+        self.df = df
+        self.int = int
+     
+    def timestwo(int):
+        
+        mult = int * 2
+        
+        return(mult)
+        
+        
+    def dividebytwo(int):
+        
+        mult = int / 2
+        
+        return(mult)       
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='wec_test',
+      version='0.1',
+      description='resolving package issues',
+      url='https://github.com/moone009/pypitest',
+      author='Christopher Mooney',
+      author_email='mooneychristopher1@gmail.com',
+      license='MIT',
+      packages=['wec_test'],
+      package_dir={'wec_test': ''},
+      zip_safe=False)
